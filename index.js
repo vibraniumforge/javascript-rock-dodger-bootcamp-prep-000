956484a0c2193abf517ed053ec5c120623e6453f
Greg Morgan <gregm9999@gmail.com>
2019-03-22 03:03:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -48,32 +48,7 @@ function createRock(x) {
       rock.remove();
     }
     window.requestAnimation(moveRock);
-
-    // implement me!
-    // (use the comments below to guide you!)
-    /**
-     * If a rock collides with the DODGER,
-     * we should call endGame()
-     */
-
-    /**
-     * Otherwise, if the rock hasn't reached the bottom of
-     * the GAME, we want to move it again.
-     */
-
-    /**
-     * But if the rock *has* reached the bottom of the GAME,
-     * we should remove the rock from the DOM
-     */
-  }
-
-  // We should kick of the animation of the rock around here
-
-  // Add the rock to ROCKS so that we can remove all rocks
-  // when there's a collision
   ROCKS.push(rock)
-
-  // Finally, return the rock element you've created
   return rock
 }
 
