4c0617475509f250c6ac4bfcc314d0194ef318a3
Greg Morgan <gregm9999@gmail.com>
2019-03-22 02:45:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -120,6 +120,7 @@ function moveDodger(e) {
    * And be sure to use the functions declared below!
    */
    if (e.which ===LEFT_ARROW) {
+     e.preventDefault();
      e.stopPropogation();
      moveDodgerLeft();
    } else if (e.which ===RIGHT_ARROW) {
